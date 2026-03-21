d4e7c4a8e4e20c72a5e7f7e13dce637fdd17e05e
Quynh Do <quynhquynh26118@gmail.com>
2026-03-21 14:50:00 +0700

    Add component NotifiProvider

--- frontend-web/src/components/notification/NotifiProvider.tsx
+++ frontend-web/src/components/notification/NotifiProvider.tsx
@@ -6,7 +6,7 @@ interface ToastContextType {
   showToast: (message: string, severity?: "success" | "error" | "warning" | "info") => void;
 }
 
-const ToastContext = createContext<ToastContextType | undefined>(undefined);
+export const ToastContext = createContext<ToastContextType | undefined>(undefined);
 
 export const NotifyProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
