873a9ec0fba2598f6352953561db4e596155f7d6
Quynh Do <quynhquynh26118@gmail.com>
2026-04-10 16:43:03 +0700

    update routes

--- frontend-web/src/routes/urls.ts
+++ frontend-web/src/routes/urls.ts
@@ -5,8 +5,13 @@ export const projectUrl = "/projects"
 export const projectDetailUrl = "/projects/:projectId";
 export const invitationAcceptUrl = "/invitations/accept";
 
+export const isProjectPath = (pathname: string, projectId: string): boolean => {
+  return pathname.startsWith(`/projects/${projectId}`);
+};
+
 export const ROUTES = {
   projectDetail: (id: string) => `/projects/${id}`,
   projectDetailsSettings: (id: string) => `/projects/${id}/settings/details`,
   projectMembersSettings: (id: string) => `/projects/${id}/settings/members`,
+  projectStatusesSettings: (id: string) => `/projects/${id}/settings/statuses`,
 };
