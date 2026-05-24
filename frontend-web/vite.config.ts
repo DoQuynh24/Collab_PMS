5873afd24ae3bc260b51986103db0a5d6431ba0b
Quynh Do <quynhquynh26118@gmail.com>
2026-05-24 15:46:38 +0700

    update vite config

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   preview: {
-    allowedHosts: ['collabpmsweb-production.up.railway.app'],
+    allowedHosts: ['all'],
   },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
